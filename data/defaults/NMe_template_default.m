adb9e8c5d0669da528e4263f4daca64f793a09ef
Oleh Krupa <olehkrupa@MBP-Oleh-Krupa.local>
2022-08-24 08:54:41 -0700

    update nme template

--- data/defaults/NMe_template_default.m
+++ data/defaults/NMe_template_default.m
@@ -1,26 +1,31 @@
 %% Evaluation template
-% Outputs are saved to a 'results' folder located in numorph's working 
+% Outputs are saved to a 'results' folder located in numorph's working
 % directory, unless specified otherwise. Note: this template only a single
 % 'update' parameter to overwrite all statistics calculated.
+update = "true";                    % Whether to overwrite previous measurements
+results_directory = [];             % Output directory for summary (default: numorph/results)
+calculate_voxel_stats = "true";     % Calculate voxel-level statistics                        
+compare_groups = ["WT","KO"];      % Select groups to compare. Defined by the 2nd value in sample 'group' variable
 
 % Specify which structures to compare
-update = "true";                                         % Whether to overwrite previous measurements
-results_directory = [];                                  % Output directory for summary (default: numorph/results)
-compare_structures_by = "table";                         % index, table; Compare all unique annotations (index), structures according to table (table)
-structure_table = "harris_cortical_groupings_w_iso.xls"; % table in /annotations/custom_annotations indicating structures to evaluate
-measure_cortex = "true";                                 % Optional: Take cortex measurments. Will only work with default ccfv3 annotations
+annotation_file = "ccfv3.mat";        % name of annotation data file from NMa_template
+template_file = "structure_template.csv";  % table in /annotations containing all possible structure indexes present (if comparing by table)
+compare_structures_by = "index";        % index, table; Compare all unique annotations (index), structures according to table (table)
+structure_table = [];                   % For comparing by "table": .xls table in /annotations/custom_annotations indicating structures to evaluate. All indexes must exist in structure_template
 
 % For cell counting, specify cell-type class information
-use_classes = "true";                           % Use cell-type classes. Otherwise raw centroid counts will be used
-keep_classes = 1:3;                             % If using classes, select which ones to evaluate by specifying the class index
-class_names = [];                               % Assign names to classes
-sum_all_classes = "true";                       % "true","false"; Create class that sums all classes detected (i.e. all nuclei)
-custom_class = ["CT1./(CT1+CT2+CT3)",...        % Create a function to combine multiple classes (i.e. calculate cell-type proportions). Specify as class names
-    "CT2./(CT1+CT2+CT3)",...
-    "CT3./(CT1+CT2+CT3)",...
-    "CT2+CT3"];                              
-minimum_cell_number = 10;                        % Minimum number of cells for each structure to be compared
+use_classes = "true";               % Use cell-type classes. Otherwise raw centroid counts will be used
+keep_classes = 1:3;                 % If using classes, select which ones to evaluate by specifying the class index
+class_names = [];                   % Assign names to each class. Otherwise, leave empty and class indexes are prefixed with "CT"
+minimum_cell_number = 10;           % Minimum number of cells for each structure to be compared
+custom_class = [];                  % Specify a function to combine multiple classes (i.e. sum 2 classes or calculate cell-type proportions)
 
-% Specify groups for comparisons
-compare_groups = ["WW","FF"];                   % Select groups to compare. Defined by the 2nd value in sample 'group' variable
-paired = "false";                               % Performed paired t-test. Samples paired by the 3rd value in sample 'group' variable
+% Custom class examples:
+% This calculates the proportion of cell-type 1 to 3 other cell-types:
+% custom_class = ["CT1/(CT1+CT2+CT3"]; 
+%
+% This calculates the sum of cell-types 2+3
+% custom_class = ["CT2+CT3"];
+%
+% To do both:
+% custom_class = ["CT1/(CT1+CT2+CT3", "CT2+CT3"];
